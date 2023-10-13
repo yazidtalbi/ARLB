ad289674ee8e60aaef9ab349071b4931190bb4c7
Yazid <mti.yazid@gmail.com>
2023-10-13 04:21:28 +0100

    optimisation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import compressor from "astro-compressor";
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), compressor({ gzip: true, brotli: false })],  
+  headers: [
+    {
+      source : "**/*.@(gz|gzip)",
+      headers : [{
+        "key" : "Content-Encoding",
+        "value" : "gzip"
+      }]
+    }
+  ]
+
 });
